58dcfd7361d1081d7d256ab958aa91614ea96b45
TupiNUMBooR <TupiNUMBooR@gmail.com>
2023-02-16 15:24:30 +1000

    Fix calculation and arrays bugs

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -9,6 +9,7 @@ import java.time.LocalTime;
 import java.time.Month;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -24,16 +25,15 @@ public class UserMealsUtil {
 
         List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
         mealsTo.forEach(System.out::println);
-        System.out.println("");
-        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
+        System.out.println();
+        filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000)
+                .forEach(System.out::println);
     }
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         Map<LocalDate, List<UserMeal>> dateMeals = new HashMap<>(); // я не знаю, упорядочен ли meals или нет, так что предполагаем худший сценарий
 
         for (UserMeal meal : meals) {
-            if (!isLocalTimeBetween(meal.getDateTime().toLocalTime(), startTime, endTime)) continue;
-
             LocalDate date = meal.getDateTime().toLocalDate();
             if (!dateMeals.containsKey(date)) dateMeals.put(date, new ArrayList<>());
             dateMeals.get(date).add(meal);
@@ -44,15 +44,17 @@ public class UserMealsUtil {
             int currentCalories = 0;
             for (UserMeal meal : entry.getValue())
                 currentCalories += meal.getCalories();
-            for (UserMeal meal : meals)
+            for (UserMeal meal : entry.getValue()) {
+                if (!isLocalTimeBetween(meal.getDateTime().toLocalTime(), startTime, endTime)) continue;
                 mealsWithExcess.add(new UserMealWithExcess(meal, currentCalories > caloriesPerDay));
+            }
         }
+
         return mealsWithExcess;
     }
 
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         Map<LocalDate, List<UserMeal>> dateMeals = meals.stream()
-                .filter(meal -> isLocalTimeBetween(meal.getDateTime().toLocalTime(), startTime, endTime))
                 .collect(Collectors.groupingBy(meal -> meal.getDateTime().toLocalDate())); // я не знаю, упорядочен ли meals или нет, так что предполагаем худший сценарий
 
         List<UserMealWithExcess> mealsWithExcess = dateMeals.entrySet().stream()
@@ -61,16 +63,18 @@ public class UserMealsUtil {
                     int currentCalories = entry.getValue().stream()
                             .mapToInt(UserMeal::getCalories)
                             .sum();
-                    return meals.stream()
+                    return entry.getValue().stream()
+                            .sorted(Comparator.comparing(UserMeal::getDateTime))
+                            .filter(meal -> isLocalTimeBetween(meal.getDateTime().toLocalTime(), startTime, endTime))
                             .map(meal -> new UserMealWithExcess(meal, currentCalories > caloriesPerDay));
-                }).collect(Collectors.toList());
-
+                })
+                .collect(Collectors.toList());
         return mealsWithExcess;
         // можно конечно обойтись без Map, упорядочив значения перед проверкой, но я не уверен что из этого быстрее
     }
 
     private static boolean isLocalTimeBetween(LocalTime time, LocalTime left, LocalTime right) {
-        return time.compareTo(left) >= 0 || time.compareTo(right) <= 0;
+        return time.compareTo(left) >= 0 && time.compareTo(right) <= 0;
     }
 
     private static void fillMealsByStreams(List<UserMeal> meals, List<UserMealWithExcess> mealsWithExcess, boolean excess) {
