3425f339a2a2c357dd71c5790d241f56b7ac691d
TupiNUMBooR <TupiNUMBooR@gmail.com>
2023-03-25 19:13:50 +1000

    Add custom date time formatters

---
+++ src/main/java/ru/javawebinar/topjava/util/formatters/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package ru.javawebinar.topjava.util.formatters;
+
+import org.springframework.lang.NonNullApi;
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,6 +1,5 @@
 package ru.javawebinar.topjava.web.meal;
 
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.formatters.CustomDateTimeFormat;
+import ru.javawebinar.topjava.util.formatters.CustomDateTimeFormat.Type;
 
 import java.net.URI;
 import java.time.LocalDate;
@@ -27,10 +28,10 @@ public class MealRestController extends AbstractMealController {
 
     @Override
     @GetMapping("/filter")
-    public List<MealTo> getBetween(@RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
-                                   @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.TIME) LocalTime startTime,
-                                   @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate,
-                                   @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.TIME) LocalTime endTime) {
+    public List<MealTo> getBetween(@RequestParam(required = false) @CustomDateTimeFormat(Type.DATE) LocalDate startDate,
+                                   @RequestParam(required = false) @CustomDateTimeFormat(Type.TIME) LocalTime startTime,
+                                   @RequestParam(required = false) @CustomDateTimeFormat(Type.DATE) LocalDate endDate,
+                                   @RequestParam(required = false) @CustomDateTimeFormat(Type.TIME) LocalTime endTime) {
         return super.getBetween(startDate, startTime, endDate, endTime);
     }
 
